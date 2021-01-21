edecd4b10ea48a398d45895a5c7d5bcb8221afea
Emmanuel Mtera <52101851+Emt-tz@users.noreply.github.com>
2021-01-21 19:02:53 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ function send_email(){
   var message = document.getElementById('message').value;
   var url = "https://api.vijanatech.com/email_api?name="+name+"&email="+email+"&subject="+subject+"&message="+message;
   fetch(url,{mode:'no-cors',credentials:'same-origin'})
-  .then(response => response.text())
+  .then(response => JSON.stringify(response.json()))
   .then(data => console.log(data));
   
 }
